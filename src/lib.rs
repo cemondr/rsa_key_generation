f4409214c2f1ca911cff55b3e367970bfbb8f8e6
cemondr <cemconder@gmail.com>
2020-01-25 17:55:27 -0800

    encrypt implemented: tests pass

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,31 @@
+#![allow(dead_code)]
 use toy_rsa_lib::*;
-#[allow(dead_code)]
+
 
 
 fn lambda(p: u64, q: u64) -> u64{
     lcm(p-1,q-1)
 }
-
+fn encrypt(key: u64, msg: u64) -> u64{
+    let e = 65537u64;
+    modexp(key,e,msg)
+}
 #[test]
 fn test_lambda(){
     assert_eq!(lambda(9,13),24)
 }
+#[test]
+fn test_lambda_2(){
+    assert_eq!(lambda(22,24),483)
+}
 
 #[test]
-fn test_labmda_2(){
-    assert_eq!(lambda(22,24),483)
+fn test_encrypt(){
+    let e = 65537u64;
+    assert_eq!(modexp(349,e,9),4)
+}
+#[test]
+fn test_encrypt_2(){
+    let e = 65537u64;
+    assert_eq!(modexp(214,e,42),16)
 }
