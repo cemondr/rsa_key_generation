1a05b8a4790cbd422ba4b5b104f689bb4a1a184e
cemondr <cemconder@gmail.com>
2020-01-26 00:16:52 -0800

    new test directory and modules

--- src/lib.rs
+++ src/lib.rs
@@ -5,52 +5,34 @@ use toy_rsa_lib::*;
 
 pub const EXP: u64 = 65_537;
 
+/**calculates and returns the least common divisor of key(p-1,q-1)
+ * 
+ * 
+ * tests follow immediately afterwards*/
 pub fn lambda(p: u64, q: u64) -> u64{
     lcm(p-1,q-1)
 }
-#[test]
-fn test_lambda(){
-    assert_eq!(lambda(9,13),24)
-}
-#[test]
-fn test_lambda_2(){
-    assert_eq!(lambda(22,24),483)
-}
-
-
+/**encrypts by using the PowerMod function provided by the toy_rsa_lib  
+ * 
+ * 
+ * tests follow immmediately afterwards*/
 pub fn encrypt(key: u64, msg: u32) -> u64{
     modexp(key,EXP,msg as u64)
 }
-#[test]
-fn test_encrypt(){
-    assert_eq!(modexp(349,EXP,9),4)
-}
-#[test]
-fn test_encrypt_2(){
-    assert_eq!(modexp(214,EXP,42),16)
-}
-
+/**given a key pair and a message, it firsts calculates the inverse of 𝜆(p, q) mod E
+ * and then calculates the power mod : msg^d mod (p ⋅ q) . returns decrypted message
+ * 
+ * 
+ * tests follow immediately afterwards*/ 
 pub fn decrypt(key:(u32,u32), msg:u64) -> u64{
     let x = key.0 as u64;
     let y = key.1 as u64;
     let d = modinverse(lambda(x,y), EXP);
     modexp(msg,d,x*y)
 } 
-#[test]
-fn test_decrypt(){
-    let test_key = (4,10);
-    let test_message = 23;
-    //rustc infers type based on decrypt interface
-    assert_eq!(decrypt(test_key,test_message), 9)
-}
-#[test]
-fn test_decrypt2(){
-    let test_key = (831,991);
-    let test_message = 17;
-    assert_eq!(decrypt(test_key,test_message),150586)
-}
-
-
+/**Generates a pair of primes with the help of toy_rsa library
+ * 
+ * tests whether each element of the pair received is a prime*/
 pub fn genkey() -> (u32,u32){
     let mut p = 2;
     let mut q = 2;
@@ -63,10 +45,4 @@ pub fn genkey() -> (u32,u32){
         q = rsa_prime();
     }
     (p,q)
-}
-
-#[test]
-fn count_genkey_loop(){
-    let (x,y) = genkey();
-    print!("received {:?}", (x,y))
 }
---
+++ tests/libtests.rs
@@ -0,0 +1,46 @@
+#[cfg(test)]
+use rsa_key_generator::*;
+use rug::integer::IsPrime;
+use rug::Integer;
+
+#[test]
+fn test_lambda(){
+    assert_eq!(lambda(9,13),24)
+}
+#[test]
+fn test_lambda_2(){
+    assert_eq!(lambda(22,24),483)
+}
+
+#[test]
+fn test_encrypt(){
+    assert_eq!(encrypt(349,9),4)
+}
+#[test]
+fn test_encrypt_2(){
+    assert_eq!(encrypt(214,42),16)
+}
+
+#[test]
+fn test_decrypt(){
+    let test_key = (4,10);
+    let test_message = 23;
+    //rustc infers type based on decrypt interface
+    assert_eq!(decrypt(test_key,test_message), 9)
+}
+#[test]
+fn test_decrypt2(){
+    let test_key = (831,991);
+    let test_message = 17;
+    assert_eq!(decrypt(test_key,test_message),150586)
+}
+
+#[test]
+fn count_genkey_loop(){
+    let key = genkey();
+    print!("received {:?}", (key.0,key.1));
+    let p = Integer::from(key.0);
+    let q = Integer::from(key.1);
+    assert_eq!(p.is_probably_prime(15), IsPrime::Yes);
+    assert_eq!(q.is_probably_prime(15), IsPrime::Yes);
+}
