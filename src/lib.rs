a6b320acfa1f64af7f2231bf48e51bd6f0ba3dbf
cemondr <cemconder@gmail.com>
2020-01-25 17:38:28 -0800

    lambda implemented:test pass

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,17 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use toy_rsa_lib::*;
+#[allow(dead_code)]
+
+
+fn lambda(p: u64, q: u64) -> u64{
+    lcm(p-1,q-1)
+}
+
+#[test]
+fn test_lambda(){
+    assert_eq!(lambda(9,13),24)
+}
+
+#[test]
+fn test_labmda_2(){
+    assert_eq!(lambda(22,24),483)
 }
