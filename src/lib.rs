1198b2086369dc6a83d0f4d6228e930ddca74dac
cemondr <cemconder@gmail.com>
2020-01-25 22:36:01 -0800

    implement decrypt: tests pass

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
+/* Provides ability to RSA encryption and decryption procedure  */
+
 #![allow(dead_code)]
 use toy_rsa_lib::*;
 
+pub const EXP: u64 = 65_537;
 
-
-fn lambda(p: u64, q: u64) -> u64{
+pub fn lambda(p: u64, q: u64) -> u64{
     lcm(p-1,q-1)
-}
-fn encrypt(key: u64, msg: u64) -> u64{
-    let e = 65537u64;
-    modexp(key,e,msg)
 }
 #[test]
 fn test_lambda(){
@@ -19,13 +17,35 @@ fn test_lambda_2(){
     assert_eq!(lambda(22,24),483)
 }
 
+
+pub fn encrypt(key: u64, msg: u32) -> u64{
+    modexp(key,EXP,msg as u64)
+}
 #[test]
 fn test_encrypt(){
-    let e = 65537u64;
-    assert_eq!(modexp(349,e,9),4)
+    assert_eq!(modexp(349,EXP,9),4)
 }
 #[test]
 fn test_encrypt_2(){
-    let e = 65537u64;
-    assert_eq!(modexp(214,e,42),16)
+    assert_eq!(modexp(214,EXP,42),16)
+}
+
+pub fn decrypt(key:(u32,u32), msg:u64) -> u64{
+    let x = key.0 as u64;
+    let y = key.1 as u64;
+    let d = modinverse(lambda(x,y), EXP);
+    modexp(msg,d,x*y)
+} 
+#[test]
+fn test_decrypt(){
+    let test_key = (4,10);
+    let test_message = 23;
+    //rustc infers type based on decrypt interface
+    assert_eq!(decrypt(test_key,test_message), 9)
+}
+#[test]
+fn test_decrypt2(){
+    let test_key = (831,991);
+    let test_message = 17;
+    assert_eq!(decrypt(test_key,test_message),150586)
 }
